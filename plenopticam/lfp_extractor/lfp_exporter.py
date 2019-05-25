592ff9646e05d5e757bad74b9c467a2af9858929
hahnec <inbox@christopherhahne.de>
2019-05-25 16:14:46 +0200

    feat(export): provide file-naming for sub-pixel precise shift and integration

--- plenopticam/lfp_extractor/lfp_exporter.py
+++ plenopticam/lfp_extractor/lfp_exporter.py
@@ -64,8 +64,13 @@ def export_refo_stack(refo_stack, cfg, type='tiff'):
 
     for i, refo_img in enumerate(refo_stack):
 
+        # get depth plane number for filename
+        a = range(*cfg.params[cfg.ran_refo])[i]
+        # account for sub-pixel precise depth value
+        a = round(a/patch_len, 2) if cfg.params[cfg.opt_refi] else a
+
         # write image file
-        misc.save_img_file(refo_img, os.path.join(folder_path, str(range(*cfg.params[cfg.ran_refo])[i])), type=type)
+        misc.save_img_file(refo_img, os.path.join(folder_path, str(a)), type=type)
 
     return True
 
