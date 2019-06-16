403960340a0a5929aea6c69fcde75549a14453cf
hahnec <inbox@christopherhahne.de>
2019-06-17 00:48:19 +0200

    chore(license): add license preamble

--- setup.py
+++ setup.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 
+__author__ = "Christopher Hahne"
+__email__ = "inbox@christopherhahne.de"
+__license__ = """
+    Copyright (c) 2019 Christopher Hahne <inbox@christopherhahne.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 from setuptools import setup, find_packages
 from plenopticam import __version__
-from sys import platform    # cross-platform
+from sys import platform
 
 
 APP = ['plenopticam/gui/top_level.py']
