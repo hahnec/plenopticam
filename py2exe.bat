0d29ea37106b9939eab8c71731570e07d81e0b6e
chahne <inbox@christopherhahne.de>
2019-08-17 11:02:40 +0200

    style(app): more readable script file

--- py2exe.bat
+++ py2exe.bat
@@ -4,10 +4,10 @@
 
 :: run pyinstaller with provided options
 pyinstaller plenopticam\gui\top_level.py^
-    --onefile^
+    	--onefile^
 	--noconsole^
-	--add-data C:\Python\Python36\lib\site-packages\libtiff;.\libtiff^
 	--name=plenopticam^
 	--icon=plenopticam\gui\icns\1055104.ico^
+	--add-data C:\Python\Python36\lib\site-packages\libtiff;.\libtiff^
 	--add-data=.\docs\build\html\;.\docs\build\html\^
 	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\
