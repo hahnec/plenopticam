14424522c916d7ad26c064ad91d72a8c71d019b8
hahnec <inbox@christopherhahne.de>
2019-10-04 00:30:36 +0200

    feat(version): increment version number

--- plenopticam/__init__.py
+++ plenopticam/__init__.py
@@ -16,7 +16,7 @@ Copyright (c) 2019 Christopher Hahne <inbox@christopherhahne.de>
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from . import misc
 from . import cfg
