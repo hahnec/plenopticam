edba4366ba59a602d8d93fb8c770166ee82b3838
hahnec <inbox@christopherhahne.de>
2019-11-10 21:03:42 +0100

    feat(color): add MKL method

--- plenopticam/lfp_extractor/lfp_color_eq.py
+++ plenopticam/lfp_extractor/lfp_color_eq.py
@@ -24,6 +24,7 @@ __license__ = """
 import numpy as np
 
 from plenopticam.lfp_extractor import LfpViewpoints
+from plenopticam import misc
 
 try:
     from scipy.signal import medfilt
@@ -39,14 +40,22 @@ class LfpColorEqualizer(LfpViewpoints):
 
         self._ref_img = kwargs['ref_img'] if 'ref_img' in kwargs else self.central_view
 
-        self.proc_type = 'proc_ax_prop'
+        self.proc_type = 'proc_ax_prop'#'proc_vp_arr'#
+        self.method = kwargs['method'] if 'method' in kwargs else '..'
 
     def main(self):
 
+        if self.method == 'hist_match':
+            fun = self.hist_match
+        else:
+            fun = self.color_transfer_MKL
+
+        self.proc_vp_arr(fun=self.hist_match, ref=self._ref_img, msg='Color equalization')
         if self.proc_type == 'proc_vp_arr':
-            self.proc_vp_arr(self.hist_match, ref=self._ref_img, msg='Color equalization')
+            self.proc_vp_arr(fun=fun, ref=self._ref_img, msg='Color equalization')
         elif 'proc_ax_prop':
-            self.proc_ax_propagate_2d(fun=self.hist_match, msg='Color equalization')
+            self.proc_ax_propagate_2d(fun=fun, msg='Color equalization')
+        self.proc_vp_arr(fun=self.hist_match, ref=self._ref_img, msg='Color equalization')
 
     @staticmethod
     def hist_match(src, ref):
@@ -76,3 +85,47 @@ class LfpColorEqualizer(LfpViewpoints):
             result[..., ch] = interp_vals[src_idxs].reshape(src[..., ch].shape)
 
         return result
+
+    def color_transfer_MKL(self, I0, I1):
+
+        if (I0.shape[2] != 3):
+            print('pictures must have 3 dimensions')
+
+        X0 = np.reshape(I0, [-1, I0.shape[2]])
+        X1 = np.reshape(I1, [-1, I1.shape[2]])
+
+        A = np.cov(X0.T)
+        B = np.cov(X1.T)
+
+        T = self.MKL(A, B)
+
+        mX0 = np.repeat(np.mean(X0, axis=0)[..., np.newaxis], X0.shape[0], axis=1).T
+        mX1 = np.repeat(np.mean(X1, axis=0)[..., np.newaxis], X1.shape[0], axis=1).T
+
+        XR = np.dot((X0 - mX0), T) + mX1
+
+        IR = np.reshape(XR, I0.shape)
+
+        IR = misc.Normalizer(IR).uint16_norm()
+
+        return IR
+
+    def MKL(self, A, B):
+        # N = A.shape[0]
+        [Da2, Ua] = np.linalg.eig(A)
+        Ua = np.array([Ua[:, 2] * -1, Ua[:, 1], Ua[:, 0] * -1]).T
+        # Da2 = np.diag(Da2)
+        Da2[Da2 < 0] = 0
+        # Da = np.diag(np.sqrt(Da2) + np.spacing(1))  # + eps
+        Da = np.diag(np.sqrt(Da2[::-1]))
+        C = np.dot(Da, np.dot(Ua.T, np.dot(B, np.dot(Ua, Da))))
+        [Dc2, Uc] = np.linalg.eig(C)
+        # Uc = np.array([Uc[:, 2] * -1, Uc[:, 1], Uc[:, 0] * -1]).T
+        # Dc2 = np.diag(Dc2)
+        Dc2[Dc2 < 0] = 0
+        # Dc = np.diag(np.sqrt(Dc2) + np.spacing(1))    #  + eps
+        Dc = np.diag(np.sqrt(Dc2))  # [::-1]
+        Da_inv = np.diag(1. / (np.diag(Da)))
+        T = np.dot(Ua, np.dot(Da_inv, np.dot(Uc, np.dot(Dc, np.dot(Uc.T, np.dot(Da_inv, Ua.T))))))
+
+        return T
