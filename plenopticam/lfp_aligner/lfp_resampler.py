fix(cfa): use normalization instead of clipping

plenopticam/lfp_aligner/lfp_resampler.py
@@ -69,8 +69,10 @@ class LfpResampler(LfpMicroLenses):
                                  np.arange(window.shape[0])+mic[0]-rint(mic[0]))
 
         # treatment of interpolated values being below or above original extrema
-        patch[patch < window.min()] = window.min()
-        patch[patch > window.max()] = window.max()
+        #patch[patch < window.min()] = window.min()
+        #patch[patch > window.max()] = window.max()
+
+        patch = misc.Normalizer(img=patch).type_norm(lim_min=window.min(), lim_max=window.max())
 
         return patch
 