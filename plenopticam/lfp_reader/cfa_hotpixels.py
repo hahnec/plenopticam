143d5f89bba433d4b4d16919b8265e7322e34977
hahnec <inbox@christopherhahne.de>
2019-12-23 23:56:50 +0100

    refactor(cfa): clean up code and improve readability

--- plenopticam/lfp_reader/cfa_hotpixels.py
+++ plenopticam/lfp_reader/cfa_hotpixels.py
@@ -43,31 +43,33 @@ class CfaHotPixels(object):
 
         cum_img = np.zeros(bay_img[0::2, 0::2].shape)
 
-        for i in range(2):
-            for j in range(2):
+        for c in range(4):
+            i, j = c//2, c%2
 
-                # progress update
-                percent = (j+i*2) / 4
-                self.sta.progress(percent*100, self.cfg.params[self.cfg.opt_prnt])
+            # progress update
+            percent = c / 4
+            self.sta.progress(percent*100, self.cfg.params[self.cfg.opt_prnt])
 
-                # deduct median filtered image
-                med_img = medfilt(bay_img[i::2, j::2].copy(), kernel_size=(3, 3))
-                m_img = bay_img[i::2, j::2].copy()/bay_img[i::2, j::2].max() - med_img.copy()/med_img.max()
+            # deduct median filtered image
+            med_img = medfilt(bay_img[i::2, j::2].copy(), kernel_size=(3, 3))
+            m_img = bay_img[i::2, j::2].copy()/bay_img[i::2, j::2].max() - med_img.copy()/med_img.max()
 
-                new_img = self.rectify_candidates_channel(channel=bay_img[i::2, j::2].copy(), ref_img=m_img, med_img=med_img, n=n, sig_lev=sig_lev + 2)
-                #misc.save_img_file(new_img, file_path=os.path.join(self.cfg.exp_path, 'outlier'+str(i+2*j)+'.tiff'))
+            new_img = self.rectify_candidates_channel(channel=bay_img[i::2, j::2].copy(),
+                                                      ref_img=m_img, med_img=med_img, n=n, sig_lev=sig_lev + 2)
 
-                diff_img = bay_img[i::2, j::2].copy() - new_img
-                cum_img[diff_img != 0] = 1
+            diff_img = bay_img[i::2, j::2].copy() - new_img
+            cum_img[diff_img != 0] = 1
 
-                bay_img[i::2, j::2] = new_img
+            bay_img[i::2, j::2] = new_img
 
-                # check interrupt status
-                if self.sta.interrupt:
-                    return False
+            # check interrupt status
+            if self.sta.interrupt:
+                return False
 
-        hotp_num = len(cum_img[cum_img != 0])
-        misc.save_img_file(cum_img, file_path=os.path.join(self.cfg.exp_path, str(hotp_num)+'_cum.png'))
+        # export hotpixel map
+        if self.cfg.params[self.cfg.opt_dbug]:
+            hotp_num = len(cum_img[cum_img != 0])
+            misc.save_img_file(cum_img, file_path=os.path.join(self.cfg.exp_path, 'hotpixel_map_'+str(hotp_num)+'.png'))
 
         # progress update
         self.sta.progress(100, self.cfg.params[self.cfg.opt_prnt])
@@ -79,15 +81,11 @@ class CfaHotPixels(object):
         ref_img = channel if ref_img is None else ref_img
         med_img = channel if med_img is None else med_img
 
-        # pre-select outlier candidates to narrow-down search area and speed-up the process
+        # pre-select outlier candidates (narrow-down search area and speed-up the process)
         m_val = np.mean(ref_img)
         s_val = np.std(ref_img)
         candidate_idxs = np.where((ref_img < m_val - s_val * sig_lev) | (ref_img > m_val + s_val * sig_lev))
         candidates = list(zip(candidate_idxs[0], candidate_idxs[1]))
-
-        #k = np.zeros(ref_img.shape)
-        #k[(ref_img < m_val - s_val * sig_lev) | (ref_img > m_val + s_val * sig_lev)] = 1
-        #misc.save_img_file(k, file_path=os.path.join(self.cfg.exp_path+'candidates.png'))
 
         for num, idx in enumerate(candidates):
 
@@ -99,12 +97,8 @@ class CfaHotPixels(object):
                 s_val = np.std(win)
                 if channel[j, i] < m_val - s_val * sig_lev or channel[j, i] > m_val + s_val * sig_lev:
 
-                    #num_hi = len(win[win > m_val + s_val * (sig_lev-2)])
-                    #num_lo = len(win[win < m_val - s_val * (sig_lev-2)])
-                    #if num_hi < n//2 or num_lo < n//2:
-
-                    # replace outlier by average of all directly adjacent pixels
-                    channel[j, i] = med_img[j, i]#(sum(sum(channel[j-1:j+2, i-1:i+2])) - channel[j, i]) / 8.#
+                    # replace outlier
+                    channel[j, i] = med_img[j, i]
 
             # check interrupt status
             if self.sta.interrupt:
