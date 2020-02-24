62046c4a6e8f80abd0f7a026de61ad0152a5906d
chahne <inbox@christopherhahne.de>
2020-02-24 19:40:39 +0100

    feat(v): version increment

--- plenopticam/__init__.py
+++ plenopticam/__init__.py
@@ -16,7 +16,7 @@ Copyright (c) 2019 Christopher Hahne <inbox@christopherhahne.de>
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.3.1'
+__version__ = '0.4.0'
 
 from . import misc
 from . import cfg
