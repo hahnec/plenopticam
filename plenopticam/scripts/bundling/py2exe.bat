3f47e3ce17c3495ae58cf64c5f5487036e1dcd45
chahne <inbox@christopherhahne.de>
2020-02-25 19:21:26 +0100

    refactor(scripts): re-organize scripts location

---
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -0,0 +1,13 @@
+:: remove build directories
+@RD /S /Q build
+@RD /S /Q dist
+
+:: run pyinstaller with provided options
+pyinstaller ..\..\gui\top_level.py^
+    	--onefile^
+	--noconsole^
+	--name=plenopticam^
+	--icon=..\..\gui\icns\1055104.ico^
+	--add-data C:\Python\Python36\lib\site-packages\imageio;.\imageio^
+	--add-data=..\..\..\docs\build\html\;.\docs\build\html\^
+	--add-data=..\..\gui\icns\1055104.ico;.\icns\
