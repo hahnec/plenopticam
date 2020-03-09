5c7053449984ce59ec0854ae67ef7bfc18db4213
chahne <inbox@christopherhahne.de>
2020-03-09 22:15:20 +0100

    docs(update): update documentation to current version

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,9 +1,8 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1',
+    VERSION: '0.3.4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
