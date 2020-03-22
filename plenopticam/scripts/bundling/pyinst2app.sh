e1cfcb941aff5d88e80d275ea4310f6e4a91ca4d
hahnec <inbox@christopherhahne.de>
2020-03-22 18:37:12 +0100

    feat(bundle): use pyinstaller for macOS bundle

--- plenopticam/scripts/bundling/pyinst2app.sh
+++ plenopticam/scripts/bundling/pyinst2app.sh
@@ -24,9 +24,48 @@ pyinstaller plenopticam/gui/top_level.py \
 	--icon=plenopticam/gui/icns/1055104.icns \
 	--add-data=/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/imageio/:./imageio \
 	--add-data=./docs/build/html/:./docs/build/html/ \
-	--add-data=plenopticam/gui/icns/1055104.gif:./icns/ \
-    --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' \
-    --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl'
+    --exclude-module=matplotlib \
+    --osx-bundle-identifier='org.pythonmac.unspecified.plenopticam' \
+    --add-binary='/Library/Frameworks/Python.framework/Versions/3.6/lib/libtk8.6.dylib':'tk' \
+    --add-binary='/Library/Frameworks/Python.framework/Versions/3.6/lib/libtcl8.6.dylib':'tcl' \
+    --add-data=plenopticam/cfg/cfg.json:cfg
+#    --add-data='./plenopticam/cfg/cfg.json':'Resources/cfg' \
+#    --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' \
+#    --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' \
+
+# extract version number from python file
+version=$(sed -ne 's@__version__ = \([^]]*\)@\1@gp' plenopticam/__init__.py)
+
+# add config to spec file
+sudo sed -i -e '$ d' ./plenopticam.spec
+echo "             bundle_identifier=None," >> ./plenopticam.spec
+echo "             info_plist={" >> ./plenopticam.spec
+echo "              'NSHighResolutionCapable': 'True'," >> ./plenopticam.spec
+echo "              'PyRuntimeLocations': $version," >> ./plenopticam.spec
+echo "              'CFBundleShortVersionString': $version," >> ./plenopticam.spec
+echo "              'CFBundleVersion': $version" >> ./plenopticam.spec
+echo "             }," >> ./plenopticam.spec
+echo "            )" >> ./plenopticam.spec
+
+# re-run pyinstaller with extended spec file
+sudo pyinstaller plenopticam.spec
+
+# add plist file to app bundle (as an alternative to modifying the spec file)
+#cp -f ./plenopticam/scripts/bundling/Info.plist dist/plenopticam.app/Contents/Info.plist
+
+#pyinstaller ./plenopticam.spec
+sudo mkdir ./dist/plenopticam.app/Contents/Resources/cfg
+sudo cp ./plenopticam/cfg/cfg.json ./dist/plenopticam.app/Contents/Resources/cfg/cfg.json
+
+# grant write privileges to config file
+sudo chmod -R 666 ./dist/plenopticam.app/Contents/Resources/cfg/cfg.json
+
+sudo cp -r ./docs ./dist/plenopticam.app/Contents/Resources/
+sudo mkdir -p ./dist/plenopticam.app/Contents/Resources/gui/
+sudo cp -r ./plenopticam/gui/icns ./dist/plenopticam.app/Contents/Resources/gui/
+
+# certificate signature
+sudo codesign --deep --signature-size 9400 -f -s "hahnec" ./dist/plenopticam.app
 
 # create dmg (requires npm and create-dmg)
 sudo xcode-select -switch "/Applications/Xcode.app/Contents/Developer/"
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ OPTIONS = {
     "iconfile": 'plenopticam/gui/icns/1055104.icns',
     "excludes": ['matplotlib'],
     "plist": dict(NSHumanReadableCopyright='2020 Christopher Hahne'),
-    "packages": ['numpy', 'scipy', 'colour_demosaicing', 'colour', 'color_matcher', 'imageio', 'docutils', 'pillow'],
+    "packages": ['numpy', 'scipy', 'colour_demosaicing', 'colour', 'color_matcher', 'imageio', 'docutils', 'PIL'],
 }
 
 if platform == 'darwin':
