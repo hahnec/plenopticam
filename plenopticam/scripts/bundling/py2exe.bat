264b42b15ddbeb9de4ed8d9298d1844540db3283
hahnec <inbox@christopherhahne.de>
2020-03-29 19:31:37 +0200

    refactor(travis): use option to omit confirmations

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -10,4 +10,5 @@ pyinstaller plenopticam\gui\top_level.py^
 	--icon=plenopticam\gui\icns\1055104.ico^
 	--add-data C:\Python38\lib\site-packages\imageio;.\imageio^
 	--add-data=docs\build\html\;.\docs\build\html\^
-	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\
+	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\^
+	--noconfirm
