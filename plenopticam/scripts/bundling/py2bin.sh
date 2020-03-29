5d047a586ecc629a3258f350a54810788b575f2f
hahnec <inbox@christopherhahne.de>
2020-03-29 23:33:14 +0200

    feat(path): amend paths and add compression with version in file name

--- plenopticam/scripts/bundling/py2bin.sh
+++ plenopticam/scripts/bundling/py2bin.sh
@@ -14,12 +14,18 @@ pyinstaller3.cp plenopticam/gui/top_level.py \
 	--noconsole \
 	--add-data=/home/chris/.local/lib/python3.6/site-packages/imageio/:./imageio \
 	--name=plenopticam \
-	--icon=../../gui/icns/1055104.gif \
-	--add-data=../../../docs/build/html/:./docs/build/html/ \
-	--add-data=../../gui/icns/1055104.gif:./icns/
+	--icon=plenopticam/gui/icns/1055104.gif \
+	--add-data=docs/build/html/:./docs/build/html/ \
+	--add-data=plenopticam/gui/icns/1055104.gif:./icns/
 
 # change distribution folder ownership to user
 sudo chown -R chris: ./dist
 
 # set absolute path to icon file
 gio set -t string ./dist/plenopticam 'metadata::custom-icon' 'file:/home/chris/MyRepos/plenopticam/gui/icns/1055104.gif'
+
+# extract version number from python file
+version=$(sed -ne 's@__version__ = \([^]]*\)@\1@gp' plenopticam/__init__.py)
+
+# compress to archive (c-create archive; z-compress archive with gzip; v-display progress in the terminal; f-filename)
+tar -czvf plenopticam_${version}.tar.gz ./dist/plenopticam.app
