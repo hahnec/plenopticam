3ea83a7a953913faf01af8c64c103043ce8f905d
hahnec <inbox@christopherhahne.de>
2020-03-29 23:44:24 +0200

    refactor(pypi): use testpypi

--- plenopticam/scripts/bundling/pypi_automation.sh
+++ plenopticam/scripts/bundling/pypi_automation.sh
@@ -7,8 +7,8 @@ python3 -m pip install --user --upgrade twine
 python3 setup.py sdist bdist_wheel
 
 # test upload and download
-#python3 -m twine upload --repository testpypi dist/*
-#python3 -m pip install --index-url https://test.pypi.org/simple/ plenopticam
+python3 -m twine upload --repository testpypi dist/*
+python3 -m pip install --index-url https://test.pypi.org/simple/ plenopticam
 
 # production upload and download
 #python3 -m twine upload dist/*
