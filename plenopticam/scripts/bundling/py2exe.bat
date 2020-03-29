6b251842bf6d3ab9eafeade6758290f7a9341269
hahnec <inbox@christopherhahne.de>
2020-03-29 01:23:37 +0100

    refactor(py): update to python version 3.8

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -8,6 +8,6 @@ pyinstaller ..\..\gui\top_level.py^
 	--noconsole^
 	--name=plenopticam^
 	--icon=..\..\gui\icns\1055104.ico^
-	--add-data C:\Python\Python36\lib\site-packages\imageio;.\imageio^
+	--add-data C:\Python\Python38\lib\site-packages\imageio;.\imageio^
 	--add-data=..\..\..\docs\build\html\;.\docs\build\html\^
 	--add-data=..\..\gui\icns\1055104.ico;.\icns\
