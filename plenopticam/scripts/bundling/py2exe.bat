f7735a1bfc2bc7c1759df27bd039962b0d65901b
hahnec <inbox@christopherhahne.de>
2020-03-29 12:50:45 +0200

    fix(travis): change relative paths

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -3,11 +3,11 @@
 @RD /S /Q dist
 
 :: run pyinstaller with provided options
-pyinstaller ..\..\gui\top_level.py^
+pyinstaller plenopticam\gui\top_level.py^
     	--onefile^
 	--noconsole^
 	--name=plenopticam^
-	--icon=..\..\gui\icns\1055104.ico^
+	--icon=plenopticam\gui\icns\1055104.ico^
 	--add-data C:\Python\Python38\lib\site-packages\imageio;.\imageio^
-	--add-data=..\..\..\docs\build\html\;.\docs\build\html\^
-	--add-data=..\..\gui\icns\1055104.ico;.\icns\
+	--add-data=docs\build\html\;.\docs\build\html\^
+	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\
