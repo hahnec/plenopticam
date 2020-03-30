adcc15048f78c7f6069331f962f12f164e3a105e
hahnec <inbox@christopherhahne.de>
2020-03-30 23:02:04 +0200

    refactor(travis): omit noconfirm option

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -9,8 +9,7 @@ pyinstaller plenopticam\gui\top_level.py^
 	--icon=plenopticam\gui\icns\1055104.ico^
 	--add-data C:\Python38\lib\site-packages\imageio;.\imageio^
 	--add-data=docs\build\html\;.\docs\build\html\^
-	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\^
-	--noconfirm
+	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\
 
-
+::	--noconfirm
 ::	--onefile^
