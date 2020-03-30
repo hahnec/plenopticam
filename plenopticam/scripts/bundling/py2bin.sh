239150f8e661a33cebcd0a6529deac1d64e9fbc7
hahnec <inbox@christopherhahne.de>
2020-03-30 20:43:50 +0200

    refactor(travis): amend linux build scripts for test purposes

--- plenopticam/scripts/bundling/py2bin.sh
+++ plenopticam/scripts/bundling/py2bin.sh
@@ -22,10 +22,11 @@ pyinstaller plenopticam/gui/top_level.py \
 #sudo chown -R chris: ./dist
 
 # set absolute path to icon file
-gio set -t string ./dist/plenopticam 'metadata::custom-icon' 'file:/home/chris/MyRepos/plenopticam/gui/icns/1055104.gif'
+#gio set -t string ./dist/plenopticam 'metadata::custom-icon' 'file:./plenopticam/gui/icns/1055104.gif'
 
 # extract version number from python file
 version=$(sed -ne 's@__version__ = \([^]]*\)@\1@gp' plenopticam/__init__.py)
+echo "${version}"
 
 # compress to archive (c-create archive; z-compress archive with gzip; v-display progress in the terminal; f-filename)
-tar -czvf plenopticam_${version}.tar.gz ./dist/plenopticam
+tar -czvf ./dist/plenopticam_${version}.tar.gz ./dist/plenopticam
