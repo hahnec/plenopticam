47c30a4dddc2dd52f4acfc5a693c2bd0bb0a36bb
hahnec <inbox@christopherhahne.de>
2020-03-30 22:46:35 +0200

    refactor(travis): switch to one-file mode

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -4,6 +4,7 @@
 
 :: run pyinstaller with provided options
 pyinstaller plenopticam\gui\top_level.py^
+    --onefile^
 	--name=plenopticam^
 	--icon=plenopticam\gui\icns\1055104.ico^
 	--add-data C:\Python38\lib\site-packages\imageio;.\imageio^
@@ -12,5 +13,4 @@ pyinstaller plenopticam\gui\top_level.py^
 	--noconfirm
 
 
-::    --onefile^
 ::	--noconsole^
