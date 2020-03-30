d43de27f8be67607f696ab16e4aa96a21b8d3bcd
hahnec <inbox@christopherhahne.de>
2020-03-30 22:31:49 +0200

    refactor(travis): omit pyinstaller options for test purposes

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -4,11 +4,13 @@
 
 :: run pyinstaller with provided options
 pyinstaller plenopticam\gui\top_level.py^
-    --onefile^
-	--noconsole^
 	--name=plenopticam^
 	--icon=plenopticam\gui\icns\1055104.ico^
 	--add-data C:\Python38\lib\site-packages\imageio;.\imageio^
 	--add-data=docs\build\html\;.\docs\build\html\^
 	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\^
 	--noconfirm
+
+
+::    --onefile^
+::	--noconsole^
