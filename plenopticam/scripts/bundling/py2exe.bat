9f53e63d948f0037ce43447cd277e1174fc89219
hahnec <inbox@christopherhahne.de>
2020-03-31 18:10:21 +0200

    refactor(travis): remove noconfirm option

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -4,7 +4,6 @@
 
 :: run pyinstaller with provided options
 pyinstaller plenopticam\gui\top_level.py^
-    --noconfirm^
 	--name=plenopticam^
 	--icon=plenopticam\gui\icns\1055104.ico^
 	--add-data C:\Python38\lib\site-packages\imageio;.\imageio^
@@ -13,3 +12,4 @@ pyinstaller plenopticam\gui\top_level.py^
 
 ::  --noconsole^
 ::  --onefile^
+::  --noconfirm^
