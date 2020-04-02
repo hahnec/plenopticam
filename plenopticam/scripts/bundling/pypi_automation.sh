8f0c559f906c2acb28ecb5c34246a5553aed5988
hahnec <inbox@christopherhahne.de>
2020-04-02 20:21:14 +0200

    refactor(travis): use sudo

--- plenopticam/scripts/bundling/pypi_automation.sh
+++ plenopticam/scripts/bundling/pypi_automation.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # upgrade twine
-pip3 install --upgrade twine
+sudo pip3 install --upgrade twine
 
 # bundle pypi package
-python3 setup.py sdist bdist_wheel
+sudo python3 setup.py sdist bdist_wheel
 
 # test upload and download
-python3 -m twine upload --repository testpypi dist/* -u hahnec -p $TEST_PYPI
-python3 -m pip install --index-url https://test.pypi.org/simple/ plenopticam
+sudo python3 -m twine upload --repository testpypi dist/* -u hahnec -p $TEST_PYPI
+sudo python3 -m pip install --index-url https://test.pypi.org/simple/ plenopticam
 
 # production upload and download
 #python3 -m twine upload dist/*
