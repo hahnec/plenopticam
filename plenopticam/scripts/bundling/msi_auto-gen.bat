dfb996fd7670943bc4e388fb0d405627970592b2
hahnec <inbox@christopherhahne.de>
2020-04-02 12:43:18 +0200

    feat(adv): add desktop shortcut

--- plenopticam/scripts/bundling/msi_auto-gen.bat
+++ plenopticam/scripts/bundling/msi_auto-gen.bat
@@ -18,12 +18,16 @@ set prjpath=".\plenopticam\scripts\bundling\msi_auto-gen.aip"
 :: pass version number to advanced installer
 %advinst% /edit %prjpath% /SetVersion %version%
 
+:: edit project properties
 %advinst% /edit %prjpath% /SetProperty ProductName="PlenoptiCam"
 %advinst% /edit %prjpath% /SetProperty Manufacturer="plenoptic.info"
 %advinst% /edit %prjpath% /SetProperty ARPURLINFOABOUT="https://github.com/hahnec/plenopticam/issues"
 %advinst% /edit %prjpath% /SetProperty ARPURLUPDATEINFO="https://github.com/hahnec/plenopticam/releases"
 %advinst% /edit %prjpath% /SetProperty ARPCONTACT="inbox[ät]christopherhahne.de"
 %advinst% /edit %prjpath% /SetProperty ARPHELPLINK="https://hahnec.github.io/plenopticam/build/html/index.html"
+
+:: place shortcut on desktop
+%advinst% /edit %prjpath% /NewShortcut -name PlenoptiCam -dir DesktopFolder -target APPDIR\plenopticam.exe -icon "..\..\gui\icns\1055104.ico"
 
 :: set installer output path
 python -c "import os,sys; print(os.getcwd())" > tmp.txt
