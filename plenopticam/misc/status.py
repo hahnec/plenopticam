5b25209f2e89a4f5ad54eb59b7a596453d116e93
hahnec <inbox@christopherhahne.de>
2020-04-24 18:35:42 +0200

    style(sta): remove dots from status messages

--- plenopticam/misc/status.py
+++ plenopticam/misc/status.py
@@ -66,7 +66,7 @@ class PlenopticamStatus(object):
         elif self.interrupt:
             self.prog_var = ''
         elif x is None:
-            self.prog_var = 'Processing ...'
+            self.prog_var = 'Processing'
         else:
             self.prog_var = str(x)
 
@@ -84,7 +84,7 @@ class PlenopticamStatus(object):
             if opt:
                 print('\n', msg)
         else:
-            self.stat_var = 'Canceling ...' if self._error is False else self.stat_var
+            self.stat_var = 'Canceling' if self._error is False else self.stat_var
             self.progress('', opt=False)
 
             return True
