a60f91cdcb90fd1b314df7e141f4e71d726622d8
hahnec <inbox@christopherhahne.de>
2020-05-06 00:05:55 +0200

    feat(inv): add inverse viewpoint composition to micro image array

--- plenopticam/lfp_extractor/lfp_rearranger.py
+++ plenopticam/lfp_extractor/lfp_rearranger.py
@@ -22,8 +22,8 @@ __license__ = """
 
 import numpy as np
 
-from plenopticam.misc import Normalizer
 from plenopticam.lfp_extractor import LfpViewpoints
+from plenopticam.misc import PlenopticamError
 
 
 class LfpRearranger(LfpViewpoints):
@@ -32,17 +32,38 @@ class LfpRearranger(LfpViewpoints):
         super(LfpRearranger, self).__init__(*args, **kwargs)
 
         self._lfp_img_align = lfp_img_align if lfp_img_align is not None else None
-        self._dtype = self._lfp_img_align.dtype
+        self._dtype = self._lfp_img_align.dtype if self._lfp_img_align is not None else self._vp_img_arr.dtype
 
-    def var_init(self):
+    def _init_vp_img_arr(self):
+        """ initialize viewpoint output image array """
 
-        # initialize output image array
         if len(self._lfp_img_align.shape) == 3:
             m, n, p = self._lfp_img_align.shape
+        elif len(self._lfp_img_align.shape) == 2:
+            m, n, p = self._lfp_img_align.shape[:2] + (1,)
         else:
-            m, n, p = (self._lfp_img_align.shape[0], self._lfp_img_align.shape[1], 1)
+            raise PlenopticamError('Dimensions %s of provided light-field not supported', self._lfp_img_align.shape)
 
         self._vp_img_arr = np.zeros([int(self._M), int(self._M), int(m/self._M), int(n/self._M), p], dtype=self._dtype)
+
+    def _init_lfp_img_align(self):
+        """ initialize micro image output image array """
+
+        if len(self._vp_img_arr.shape) == 5:
+            m, n, p = self._vp_img_arr.shape[2:]
+        elif len(self._vp_img_arr.shape) == 4:
+            m, n, p = self._vp_img_arr.shape[2:] + (1,)
+        else:
+            raise PlenopticamError('Dimensions %s of provided light-field not supported', self._vp_img_arr.shape)
+
+        m *= self._vp_img_arr.shape[0]
+        n *= self._vp_img_arr.shape[1]
+
+        # create empty array
+        self._lfp_img_align = np.zeros([m, n, p], dtype=self._dtype)
+
+        # update angular resolution parameter
+        self._M = self._vp_img_arr.shape[0] if self._vp_img_arr.shape[0] == self._vp_img_arr.shape[1] else float('inf')
 
     def main(self):
 
@@ -51,16 +72,20 @@ class LfpRearranger(LfpViewpoints):
             return False
 
         # rearrange light-field to viewpoint representation
-        self.viewpoint_extraction()
+        self.compose_viewpoints()
 
-    def viewpoint_extraction(self):
+    def compose_viewpoints(self):
+        """
+        Conversion from aligned micro image array to viewpoint array representation. The fundamentals behind the
+        4-D light-field transfer were derived by Levoy and Hanrahans in their paper 'Light Field Rendering' in Fig. 6.
+        """
 
         # print status
-        self.sta.status_msg('Viewpoint extraction', self.cfg.params[self.cfg.opt_prnt])
+        self.sta.status_msg('Viewpoint composition', self.cfg.params[self.cfg.opt_prnt])
         self.sta.progress(None, self.cfg.params[self.cfg.opt_prnt])
 
         # initialize basic light-field parameters
-        self.var_init()
+        self._init_vp_img_arr()
 
         # rearrange light field to multi-view image representation
         for j in range(self._M):
@@ -71,7 +96,37 @@ class LfpRearranger(LfpViewpoints):
                     return False
 
                 # extract viewpoint by pixel rearrangement
-                self._vp_img_arr[j, i, :, :, :] = self._lfp_img_align[j::self._M, i::self._M, :]
+                self._vp_img_arr[j, i, ...] = self._lfp_img_align[j::self._M, i::self._M, :]
+
+                # print status
+                percentage = (j*self._M+i+1)/self._M**2
+                self.sta.progress(percentage*100, self.cfg.params[self.cfg.opt_prnt])
+
+        return True
+
+    def decompose_viewpoints(self):
+        """
+        Conversion from viewpoint image array to aligned micro image array representation. The fundamentals behind the
+        4-D light-field transfer were derived by Levoy and Hanrahans in their paper 'Light Field Rendering' in Fig. 6.
+        """
+
+        # print status
+        self.sta.status_msg('Viewpoint decomposition', self.cfg.params[self.cfg.opt_prnt])
+        self.sta.progress(None, self.cfg.params[self.cfg.opt_prnt])
+
+        # initialize basic light-field parameters
+        self._init_lfp_img_align()
+
+        # rearrange light field to multi-view image representation
+        for j in range(self._M):
+            for i in range(self._M):
+
+                # check interrupt status
+                if self.sta.interrupt:
+                    return False
+
+                # extract viewpoint by pixel rearrangement
+                self._lfp_img_align[j::self._M, i::self._M, :] = self._vp_img_arr[j, i, :, :, :]
 
                 # print status
                 percentage = (j*self._M+i+1)/self._M**2
