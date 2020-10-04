f776160cd14d4165fe53ef7aaef6f0df6e33799d
hahnec <inbox@christopherhahne.de>
2020-10-04 22:40:10 +0200

    feat(ver): version increment

--- plenopticam/__init__.py
+++ plenopticam/__init__.py
@@ -16,7 +16,7 @@ __license__ = """
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 
 from . import misc
 from . import cfg
