0c627e549606458f6437b904256e1b1697636923
hahnec <inbox@christopherhahne.de>
2021-02-28 17:58:58 +0100

    fix(win): amend python site-packages path for imageio

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -8,7 +8,7 @@ pyinstaller plenopticam\gui\top_level.py^
     --onefile^
     --noconsole^
 	--icon=plenopticam\gui\icns\1055104.ico^
-	--add-data=C:\Python38\lib\site-packages\imageio;.\imageio^
+	--add-data=c:\hostedtoolcache\windows\python\3.8.7\x64\lib\site-packages\imageio;.\imageio^
 	--add-data=docs\build\html\;.\docs\build\html\^
 	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\
 
