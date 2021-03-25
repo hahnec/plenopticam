2e047089197f985515795b3fbc3f2368609a03a6
hahnec <inbox@christopherhahne.de>
2021-03-26 00:21:13 +0100

    fix(app): use explicit path

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -12,7 +12,7 @@ pyinstaller plenopticam\gui\top_level.py^
     --onefile^
     --noconsole^
 	--icon=plenopticam\gui\icns\1055104.ico^
-	--add-data=%sp_path%\imageio;.\imageio^
+	--add-data=c:\hostedtoolcache\windows\python\3.8.8\x64\lib\site-packages\imageio;.\imageio^
 	--add-data=docs\build\html\;.\docs\build\html\^
 	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\
 
