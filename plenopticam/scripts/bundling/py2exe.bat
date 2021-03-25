6056d1f3f388749f07c8b9d95f048d8b27158e8a
hahnec <inbox@christopherhahne.de>
2021-03-25 23:20:24 +0100

    fix(app): auto-determine site-packages path for windows bundling

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -2,13 +2,17 @@
 @RD /S /Q build
 @RD /S /Q dist
 
+:: find python's site-packages path
+set sp_path = python -c "import sysconfig; print(sysconfig.get_path('purelib'))"
+%sp_path%
+
 :: run pyinstaller with provided options
 pyinstaller plenopticam\gui\top_level.py^
 	--name=plenopticam^
     --onefile^
     --noconsole^
 	--icon=plenopticam\gui\icns\1055104.ico^
-	--add-data=c:\hostedtoolcache\windows\python\3.8.7\x64\lib\site-packages\imageio;.\imageio^
+	--add-data=%sp_path%\imageio;.\imageio^
 	--add-data=docs\build\html\;.\docs\build\html\^
 	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\
 
