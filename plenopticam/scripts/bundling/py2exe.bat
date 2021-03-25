05fe1419e645cfcf43388900741a6fd46245121a
hahnec <inbox@christopherhahne.de>
2021-03-25 23:45:48 +0100

    fix(echo): use exclamation mark for print statements

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -4,7 +4,7 @@
 
 :: find python's site-packages path
 set sp_path = python -c "import sysconfig; print(sysconfig.get_path('purelib'))"
-%sp_path%
+echo !sp_path!
 
 :: run pyinstaller with provided options
 pyinstaller plenopticam\gui\top_level.py^
@@ -12,7 +12,7 @@ pyinstaller plenopticam\gui\top_level.py^
     --onefile^
     --noconsole^
 	--icon=plenopticam\gui\icns\1055104.ico^
-	--add-data=%sp_path%\imageio;.\imageio^
+	--add-data=!sp_path!\imageio;.\imageio^
 	--add-data=docs\build\html\;.\docs\build\html\^
 	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\
 
