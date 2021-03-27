a87a922e929710d9cd55b07054577a64645b1b48
hahnec <inbox@christopherhahne.de>
2021-03-27 14:50:17 +0100

    chore(ver): version increment v0.7.0

--- plenopticam/__init__.py
+++ plenopticam/__init__.py
@@ -16,7 +16,7 @@ __license__ = """
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.6.5'
+__version__ = '0.7.0'
 
 from . import misc
 from . import cfg
