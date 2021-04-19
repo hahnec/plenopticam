147596576e39deae0323b52e21c918e8e75b256b
hahnec <inbox@christopherhahne.de>
2021-04-19 21:43:46 +0200

    fix(bat): attempt to use generic path for imageio lib

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -3,7 +3,7 @@
 @RD /S /Q dist
 
 :: find python's site-packages path
-set sp_path = python3 -c "import sysconfig; print(sysconfig.get_path('purelib'))"
+set sp_path = python -c "import site; print(site.getsitepackages()[0])"
 %sp_path%
 
 :: run pyinstaller with provided options
@@ -12,8 +12,6 @@ pyinstaller plenopticam\gui\top_level.py^
     --onefile^
     --noconsole^
 	--icon=plenopticam\gui\icns\1055104.ico^
-	--add-data=c:\hostedtoolcache\windows\python\3.8.8\x64\lib\site-packages\imageio;.\imageio^
+	--add-data="$sp_path"\imageio;.\imageio^
 	--add-data=docs\build\html\;.\docs\build\html\^
 	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\
-
-::    --noconfirm^
