36edc26a4d7c75976b87bf68a03a2a9406f7e46e
hahnec <inbox@christopherhahne.de>
2021-04-21 20:30:44 +0200

    fix(path): use explicit path

--- plenopticam/scripts/bundling/py2exe.bat
+++ plenopticam/scripts/bundling/py2exe.bat
@@ -4,7 +4,7 @@
 
 :: find python's site-packages path
 SET sp_path = python -c "import site; print(site.getsitepackages()[0])"
-ECHO !sp_path!
+%sp_path%
 
 :: run pyinstaller with provided options
 pyinstaller plenopticam\gui\top_level.py^
@@ -12,6 +12,6 @@ pyinstaller plenopticam\gui\top_level.py^
     --onefile^
     --noconsole^
 	--icon=plenopticam\gui\icns\1055104.ico^
-	--add-data=%sp_path%\imageio;.\imageio^
+	--add-data=c:\hostedtoolcache\windows\python\3.8.9\x64\lib\site-packages\imageio;.\imageio^
 	--add-data=docs\build\html\;.\docs\build\html\^
 	--add-data=plenopticam\gui\icns\1055104.ico;.\icns\
