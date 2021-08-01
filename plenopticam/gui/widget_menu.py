27aa494e5364e1be306a70419b2027d4adc4a739
hahnec <inbox@christopherhahne.de>
2021-08-01 14:28:55 +0200

    refactor(pep): minor refactoring to comply to PEP8

--- plenopticam/gui/widget_menu.py
+++ plenopticam/gui/widget_menu.py
@@ -40,49 +40,51 @@ class MenuWidget(tk.Frame):
         self.parent = parent
         self.parent.parent.createcommand('tkAboutDialog', self.open_about_dialog)
 
-        menubar = tk.Menu(self)
+        menu_bar = tk.Menu(self)
 
-        filemenu = MenuBtns(menubar, tearoff=0)
-        filemenu.add_command(label="Process", command=self.parent.process)
-        filemenu.add_command(label="Settings", command=self.parent.cfg_change)
-        filemenu.add_separator()
-        filemenu.add_command(label="Stop", command=self.parent.stop)
-        filemenu.add_command(label="Quit", command=self.parent.quit)
-        menubar.add_cascade(label="File", menu=filemenu)
+        file_menu = MenuBtns(menu_bar, tearoff=0)
+        file_menu.add_command(label="Process", command=self.parent.process)
+        file_menu.add_command(label="Settings", command=self.parent.cfg_change)
+        file_menu.add_separator()
+        file_menu.add_command(label="Stop", command=self.parent.stop)
+        file_menu.add_command(label="Quit", command=self.parent.quit)
+        menu_bar.add_cascade(label="File", menu=file_menu)
 
-        helpmenu = tk.Menu(menubar, tearoff=0)
-        helpmenu.add_command(label="About...", command=self.open_about_dialog)
-        helpmenu.add_command(label="Help...", command=self.open_docs)
-        menubar.add_cascade(label="Help", menu=helpmenu)
+        help_menu = tk.Menu(menu_bar, tearoff=0)
+        help_menu.add_command(label="About...", command=self.open_about_dialog)
+        help_menu.add_command(label="Help...", command=self.open_docs)
+        menu_bar.add_cascade(label="Help", menu=help_menu)
 
-        self.parent.parent.config(menu=menubar)
+        self.parent.parent.config(menu=menu_bar)
 
-        self.btn_list = [filemenu]
+        self.btn_list = [file_menu]
 
-    def open_about_dialog(self):
-        ''' open about window '''
+    @staticmethod
+    def open_about_dialog():
+        """ open about window """
 
         # instantiate about widget
         AbtWidget()
 
         return True
 
-    def open_docs(self):
+    @staticmethod
+    def open_docs():
         """ open documentation in webbrowser """
 
         # relative path for html documentation
-        REL_PATH = "docs/build/html/index.html"
+        rel_path = "docs/build/html/index.html"
 
         # current working directory
         cwd = os.getcwd()
 
         # compose url
-        if os.path.exists(os.path.join(cwd, REL_PATH)):
-            url = "file:///" + os.path.join(cwd, REL_PATH)
-        elif hasattr(sys, '_MEIPASS') and os.path.exists(os.path.join(sys._MEIPASS, REL_PATH)):
-            url = "file:///" + os.path.join(sys._MEIPASS, REL_PATH)
-        elif os.path.exists(os.path.join(os.path.dirname(os.path.dirname(cwd)), REL_PATH)):
-            url = "file:///" + os.path.join(os.path.dirname(os.path.dirname(cwd)), REL_PATH)
+        if os.path.exists(os.path.join(cwd, rel_path)):
+            url = "file:///" + os.path.join(cwd, rel_path)
+        elif hasattr(sys, '_MEIPASS') and os.path.exists(os.path.join(sys._MEIPASS, rel_path)):
+            url = "file:///" + os.path.join(sys._MEIPASS, rel_path)
+        elif os.path.exists(os.path.join(os.path.dirname(os.path.dirname(cwd)), rel_path)):
+            url = "file:///" + os.path.join(os.path.dirname(os.path.dirname(cwd)), rel_path)
         else:
             url = 'https://hahnec.github.io/plenopticam/build/html/index.html'
 
@@ -92,11 +94,12 @@ class MenuWidget(tk.Frame):
             webbrowser.open(url, new=2)
         else:
             # workaround for linux app bundle inspired by https://github.com/pyinstaller/pyinstaller/issues/3668
-            myEnv = dict(os.environ)
-            myEnv['XDG_DATA_DIRS'] = '/usr/local/share:/usr/share:/var/lib/snapd/desktop'
+            my_env = dict(os.environ)
+            my_env['XDG_DATA_DIRS'] = '/usr/local/share:/usr/share:/var/lib/snapd/desktop'
             import subprocess
-            subprocess.Popen(["xdg-open", url], env=myEnv)
+            subprocess.Popen(["xdg-open", url], env=my_env)
             print(url)
+
 
 class MenuBtns(tk.Menu):
     """ child of menu class that supports "state" as key for disabling menu buttons """
