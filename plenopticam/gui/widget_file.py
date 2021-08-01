df4c6b9b88e761345eaf2f2fa6631ddf6f7ca643
hahnec <inbox@christopherhahne.de>
2021-08-01 14:27:43 +0200

    refactor(ui): minor cosmetic changes to the user interface

--- plenopticam/gui/widget_file.py
+++ plenopticam/gui/widget_file.py
@@ -56,13 +56,13 @@ class FileWidget(tk.Frame):
                     ('All files', ' '.join(ALL_EXTS+[x.upper() for x in ALL_EXTS]))]
 
         # instantiate light field path widget
-        tk.Label(self, text='Light field image: ').grid(row=0, column=0, sticky='W')
+        tk.Label(self, text='Light field image').grid(row=0, column=0, sticky='W')
         self.lfp_wid = PathWidget(self, path=self.cfg.params[self.cfg.lfp_path], path_type=False, file_exts=LFP_EXTS)
         self.lfp_wid.grid(row=0, column=1, padx=PX, pady=PY)
         self.lfp_wid.bind_to(self.set_lfp_path)     # observe change in path variable
 
         # instantiate calibration path widget
-        tk.Label(self, text='Calibration source: ').grid(row=1, column=0, sticky='W')
+        tk.Label(self, text='Calibration source').grid(row=1, column=0, sticky='W')
         self.cal_wid = PathWidget(self, path=self.cfg.params[self.cfg.cal_path], path_type=False, file_exts=CAL_EXTS)
         self.cal_wid.grid(row=1, column=1, padx=PX, pady=PY)
         self.cal_wid.bind_to(self.set_cal_path)     # observe change in path variable
@@ -70,7 +70,7 @@ class FileWidget(tk.Frame):
         # radio button to enable change from path to file type
         self.cal_wid.path_type = os.path.isdir(self.cfg.params[self.cfg.cal_path])
         self.chk_var = tk.BooleanVar(value=bool(self.cal_wid.path_type))
-        self.chk_btn = tk.Checkbutton(self, text='Pick folder', variable=self.chk_var, command=self.btn_update)
+        self.chk_btn = tk.Checkbutton(self, text='Folder', variable=self.chk_var, command=self.btn_update)
         self.chk_btn.grid(row=1, column=2, sticky='W')
 
         # list of button and entry widgets (collected to disable/enable widgets)
