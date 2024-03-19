a17aa328fd6e311b05d58cd459bf16783f2abae5
hahnec <christopher.hahne@unibe.ch>
2024-03-19 15:48:20 +0100

    fix(add): add binaries

--- plenopticam/scripts/bundling/pyinst2app_gh.sh
+++ plenopticam/scripts/bundling/pyinst2app_gh.sh
@@ -25,8 +25,8 @@ pyinstaller plenopticam/gui/top_level.py \
   --exclude-module=matplotlib \
   --osx-bundle-identifier='org.pythonmac.unspecified.plenopticam' \
   --hidden-import pkg_resources.py2_warn \
-  --add-data='/usr/local/lib/libtk8.6.dylib':'lib' \
-  --add-data='/usr/local/lib/libtcl8.6.dylib':'lib' \
+  --add-binary='/usr/local/lib/libtk8.6.dylib':'tk' \
+  --add-binary='/usr/local/lib/libtcl8.6.dylib':'tcl' \
   --add-data=plenopticam/cfg/cfg.json:cfg
 
 # extract version number from python file
